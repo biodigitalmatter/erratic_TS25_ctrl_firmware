2257d334a07e7679b75f111f790c1ae062d38c93
Anton Tetov <anton@tetov.se>
2025-05-21 17:07:58 +0200

    eglot for arduino

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,10 @@
-((nil . ((eval . (add-to-list 'eglot-server-programs
-                                   '(arduino-mode .
-                                     ("arduino-language-server"
+((arduino-mode
+  (eval .
+        (progn
+          (require 'eglot)
+          (add-to-list 'eglot-server-programs
+                       '(arduino-mode . ("arduino-language-server"
                                       "-clangd" "clangd"
                                       "-cli" "arduino-cli"
                                       "-cli-config" "./arduino-cli.yaml"
-                                      "-fqbn" "controllino_rp2:rp2040:controllino_micro")))))
+                                      "-fqbn" "controllino_rp2:rp2040:controllino_micro")))))))
